ddf330c5c2a5fa816f89f7511002843ccae6d209
David Küster <davidalankuester@yahoo.de>
2020-06-29 19:52:16 +0200

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,32 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  images = ['schach.jpg', 'keyboard.jpg', 'bücherregal.jpg', 'Küster2.jpg', 'pc.jpg'];
-  headLines = [
-  'Bringing life to the next level', 
-  'Work and life balanced', 'Master of Arts', 
-  'Rising political ambitions', 
-  'Work starts at home'
-  ];
-  currentImage = 0;
-  showImage = true;
-
-  ngOnInit() {
-    this.updateImage();
-  }
-
-  updateImage() {
-
-    setInterval(() => {
-      this.currentImage++;
-      this.currentImage = this.currentImage % this.images.length;
-      this.showImage = false;
-
-      setTimeout(() => {
-        this.showImage = true;
-      }, 10);
-    }, 8000);
-  }
+export class AppComponent {
 
 }
