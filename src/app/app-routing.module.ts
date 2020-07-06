83a4a874af709f791862e27327b0188e62d7307e
David Küster <davidalankuester@yahoo.de>
2020-07-06 11:57:05 +0200

    imprint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImprintComponent } from './imprint/imprint.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 
-const routes: Routes = [];
+
+
+
+
+
+const routes: Routes = [
+  { path: '', component: SlideshowComponent },
+  { path: 'imprint', component: ImprintComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
