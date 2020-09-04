3bf96f4db9f8bf661e7673cd101b1e3a532a2f8f
David Küster <davidalankuester@yahoo.de>
2020-09-04 20:45:19 +0200

    worked Version with new attributes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,8 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-@Input() imprintMode = true;
-@Input() dataProtectionMode = true;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,39 +8,19 @@ import { Router } from '@angular/router';
 })
 export class MenuComponent implements OnInit {
   @Input() darkMode = true;
-  //@Input() scrollMode = false;
+
   constructor(public router: Router) { }
 
   ngOnInit(): void {
-    // this.scrollingChange()
   }
 
-  showDarkMode() {
-    return this.router.url.includes('/imprint') ||
-      this.router.url.includes('/data-protection') ||
-      this.router.url.includes('/cv') ||
-      this.router.url.includes('/political-science');
-  }
-
-  // scrollingChange() {
-  //   window.onscroll = function () {
-  //     if (window.pageYOffset > 0) {
-  //       this.scrollMode;
-  //       // document.getElementById('header').classList.add('scrollingHeader');
-  //       // document.getElementById('logo-white').classList.add('hide');
-  //       // document.getElementById('logo-black').classList.remove('hide');
-  //       // document.getElementById('header-links').classList.add('scrollingDarkLinks');
-
-
-  //     } else {
-  //       this.scrollMode = false;
-  //       // document.getElementById('header').classList.remove('scrollingHeader')
-  //       // document.getElementById('logo-white').classList.remove('hide');
-  //       // document.getElementById('logo-black').classList.add('hide');
-  //       // document.getElementById('header-links').classList.remove('scrollingDarkLinks');
-
-  //     }
-  //   }
+  // showDarkMode() {
+  //   return this.router.url.includes('/imprint') ||
+  //     this.router.url.includes('/data-protection') ||
+  //     this.router.url.includes('/cv') ||
+  //     this.router.url.includes('/political-science');
   // }
+
+
 
 }
